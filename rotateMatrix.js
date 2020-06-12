76b1ba08e8411de450c76a66b5e50d1e3ad6ac18
Jacobo Soffer <sofferjacob@gmail.com>
2020-06-12 10:11:26 -0500

    Create rotateMatrix.js

---
+++ rotateMatrix.js
@@ -0,0 +1,36 @@
+const printMatrix = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+class Elements {
+  constructor(arr) {
+    this.arr = arr;
+    this.x = 0;
+    this.y = 0;
+  }
+  getElement (x, y) {
+    let newY = this.arr.length - (x + 1);
+    let newX = y;
+    return this.arr[newX][newY];
+  }
+}
+
+function rotateMatrix (arr) {
+  // Create the new array
+  let newArr = new Array();
+  const fetchElement = new Elements(arr); 
+  for (let i = 0; i < arr.length; i++) {
+    //newArr[i] = new Array();
+    let row = new Array();
+    let y = arr[i];
+    for (let x = 0; x < y.length; x++) {
+      //newArr[x][y.length-(i+1)] = y[x];
+      let element = fetchElement.getElement(i, x); //getElement(arr, i, x);
+      row.unshift(element);
+    }
+    newArr.unshift(row);
+  }
+  return newArr;
+}
