4b447eab81920f35c295de87d3fb5f5f898f9cc8
Jacobo Soffer <sofferjacob@gmail.com>
2020-06-13 12:22:34 -0500

    Update rotateMatrix.js

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,34 +1,23 @@
+// Code Challenges
+// rotateMatrix.js
+// 
+// A simple function to rotate a matrix
+// Copyright (c) 2020 Jacobo Soffer.
+
 const printMatrix = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 }
 
-class Elements {
-  constructor(arr) {
-    this.arr = arr;
-    this.x = 0;
-    this.y = 0;
-  }
-  getElement (x, y) {
-    let newY = this.arr.length - (x + 1);
-    let newX = y;
-    return this.arr[newX][newY];
-  }
-}
-
-function rotateMatrix (arr) {
-  // Create the new array
+const rotateMatrix = arr => {
   let newArr = new Array();
-  const fetchElement = new Elements(arr); 
   for (let i = 0; i < arr.length; i++) {
     let row = new Array();
-    let y = arr[i];
-    for (let x = 0; x < y.length; x++) {
-      let element = fetchElement.getElement(i, x);
-      row.unshift(element);
+    for (let x = 0; x < arr.length; x++) {
+      row.push(arr[arr.length - (x + 1)][i]);
     }
-    newArr.unshift(row);
+    newArr.push(row);
   }
   return newArr;
 }
